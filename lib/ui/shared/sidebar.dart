30b04d9d2ef7c00e82e44a8e1ee2290a2ce6ff16
Jorge Hurtado <jahurtadod@outlook.com>
2021-09-23 16:18:57 -0500

    Add menu items and separator

--- lib/ui/shared/sidebar.dart
+++ lib/ui/shared/sidebar.dart
@@ -1,3 +1,5 @@
+import 'package:admin_dashboard/ui/shared/widgets/menu_item.dart';
+import 'package:admin_dashboard/ui/shared/widgets/text_separator.dart';
 import 'package:flutter/material.dart';
 
 import 'package:admin_dashboard/ui/shared/widgets/logo.dart';
@@ -14,10 +16,70 @@ class Sidebar extends StatelessWidget {
       decoration: buildBoxDecoration(),
       child: ListView(
         physics: const ClampingScrollPhysics(),
-        children: const [
-          Logo(),
-          SizedBox(height: 50),
-          
+        children: [
+          const Logo(),
+          const SizedBox(height: 50),
+          const TextSeparator(text: 'main'),
+          MenuItem(
+            text: 'Dashboard',
+            icon: Icons.compass_calibration_outlined,
+            isActive: false,
+            onPressed: () {
+              print('Dashboard');
+            },
+          ),
+          MenuItem(
+              text: 'Analytic',
+              icon: Icons.show_chart_outlined,
+              isActive: false,
+              onPressed: () {}),
+          MenuItem(
+              text: 'Categories',
+              icon: Icons.layers_outlined,
+              isActive: false,
+              onPressed: () {}),
+          MenuItem(
+              text: 'Products',
+              icon: Icons.dashboard_outlined,
+              isActive: false,
+              onPressed: () {}),
+          MenuItem(
+              text: 'Discount',
+              icon: Icons.attach_money_outlined,
+              isActive: false,
+              onPressed: () {}),
+          MenuItem(
+              text: 'Customers',
+              icon: Icons.people_alt_outlined,
+              isActive: false,
+              onPressed: () {}),
+          const SizedBox(height: 50),
+          const TextSeparator(text: 'UI Elements'),
+          MenuItem(
+              text: 'Icons',
+              icon: Icons.list_alt_outlined,
+              isActive: false,
+              onPressed: () {}),
+          MenuItem(
+              text: 'Marketing',
+              icon: Icons.mark_email_read_outlined,
+              isActive: false,
+              onPressed: () {}),
+          MenuItem(
+              text: 'Campaign',
+              icon: Icons.note_add_outlined,
+              isActive: false,
+              onPressed: () {}),
+          MenuItem(
+              text: 'Blank',
+              icon: Icons.post_add_outlined,
+              isActive: false,
+              onPressed: () {}),
+          MenuItem(
+              text: 'Logout',
+              icon: Icons.exit_to_app_outlined,
+              isActive: false,
+              onPressed: () {}),
         ],
       ),
     );
